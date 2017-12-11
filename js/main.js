8283ec80d4625eee31c14850c5278d7ef170f022
Barbara Bombachini <bbombachini@gmail.com>
2017-12-11 15:59:02 -0500

    add fetch method

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
 (function () {
   // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
   var images = document.querySelectorAll('.data-ref');
-  const httpRequest = new XMLHttpRequest();
+  //const httpRequest = new XMLHttpRequest();
   var appliedClass;
 
     function getCarData(){
+      const url = './includes/functions.php?carModel=' + this.id;
+
+      //the fetch API uses new Javascript Promise API
+      fetch(url) //do an ajax call with fetch
+        .then((resp) => resp.json()) // convert to json
+          .then((data) => {processResult(data);}) // call the process function passing the data
+            .catch(function(error){
+              //catch ANY error and report it to console
+              console.log(error);
+            });
+
       //make an AJAX call to the db; handle errors first;
-      if(!httpRequest) {
-        alert('giving up... your browser sucks!');
-        return false;
-      }
-
-      httpRequest.onreadystatechange = processRequest;
-      httpRequest.open('GET', './includes/functions.php?carModel=' + this.id);
-      httpRequest.send();
-    }
-
-    function processRequest() {
-     let reqIndicator = document.querySelector('.request-state');
-     reqIndicator.textContent = httpRequest.readyState;
-     //debugger;
-
-     if (httpRequest.readyState === XMLHttpRequest.DONE) {
-       if (httpRequest.status === 200) { // 200 means everything is awesome
-         //debugger;
-         let data = JSON.parse(httpRequest.responseText);
-         processResult(data);
-       } else {
-         alert('There was a problem with the request.');
-       }
-     }
+    //   if(!httpRequest) {
+    //     alert('giving up... your browser sucks!');
+    //     return false;
+    //   }
+    //
+    //   httpRequest.onreadystatechange = processRequest;
+    //   httpRequest.open('GET', './includes/functions.php?carModel=' + this.id);
+    //   httpRequest.send();
+    // }
+    //
+    // function processRequest() {
+    //  let reqIndicator = document.querySelector('.request-state');
+    //  reqIndicator.textContent = httpRequest.readyState;
+    //  //debugger;
+    //
+    //  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    //    if (httpRequest.status === 200) { // 200 means everything is awesome
+    //      //debugger;
+    //      let data = JSON.parse(httpRequest.responseText);
+    //      processResult(data);
+    //    } else {
+    //      alert('There was a problem with the request.');
+    //    }
+    //  }
    }
    function processResult(data){
     // console.log(data.modelName);
@@ -43,7 +54,7 @@
     images.forEach(function(car, index){
       car.classList.add('nonActive');
     });
-    //this is a template string constructor 
+    //this is a template string constructor
     document.querySelector(`#${data.model}`).classList.remove('nonActive');
 
     //different way of doing it
